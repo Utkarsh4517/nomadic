76518cc905fa2aeee03fc1846c3b43de98824c3e
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-10-07 16:21:27 +0530

    Initialized appwrite

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Client client = Client();
+  client = Client()
+      .setEndpoint("https://cloud.appwrite.io/v1")
+      .setProject("nomadic4517");
+  Account account = Account(client);
+  runApp(MyApp(account: account));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Account account;
+  const MyApp({
+    required this.account,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
